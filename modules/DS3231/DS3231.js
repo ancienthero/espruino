2db18ad2bd060aa1736adec45fdc700b204c7473
Pawel Hajduk <pawel.hajduk@gmail.com>
2017-07-11 15:58:32 +0200

    removed summer time support, it shouldn't be handled in this module

--- modules/DS3231/DS3231.js
+++ modules/DS3231/DS3231.js
@@ -1,17 +1,10 @@
 /* Copyright (c) 2017 Paweł Hajduk, based on Peter Clarke module */
 /*
 Module is to set date time and read date time from a DS3231 over I2C.
-For the daylight saving to work the time must be read at least every second so that when it occurs we write back to the module the shifted time.
 */
 
-function DS3231(i2c, options) {
+function DS3231(i2c) {
   this.i2c = i2c;
-  this.options = options || {};
-  // are we doing daylight saving time calcs?
-  if (this.options.DST===undefined)
-    this.options.DST = false;
-  // current state of daylight saving
-  this.dstStatus = false;
 }
 
 //private
@@ -37,23 +30,6 @@ function bcd2dec(val) {
   return ((val >> 4)*10+(val & 0x0F));
 }
 
-// Return whether the supplied date is part of daylight saving time or not
-DS3231.prototype.isDST = function(day,month,dow) {
-  if (!this.options.DST) return false;
-  if ((month === 3) && (dow === 1) && (day > 23)) {
-    return true;
-  }
-  if ((month === 10) && (dow === 1) && (day > 23)) {
-    return false;
-  }
-  if ((month > 3) && (month < 11)) {
-    return true;
-  }
-  else {
-    return false;
-  }
-}
-
 // Public functions
 // Set the day of the week, Sunday = 0, Monday = 1 etc..
 DS3231.prototype.setDow = function(day) {
@@ -65,7 +41,6 @@ DS3231.prototype.setDate = function(date,month,year) {
   this.i2c.writeTo(C.i2c_address,[C.dateReg, (dec2bcd(date))]);
   this.i2c.writeTo(C.i2c_address,[C.monthReg, (dec2bcd(month+1))]);
   this.i2c.writeTo(C.i2c_address,[C.yearReg, (dec2bcd(year-2000))]);
-  this.dstStatus = this.isDST(date,month,year);
 };
 
 // Set the time
@@ -87,20 +62,6 @@ DS3231.prototype.readDateTime = function (select) {
   var month = bcd2dec(data[5]);
   var year = bcd2dec(data[6]);
 
-  if (hours === 2 && minutes === 0 && seconds === 0 && this.isDST(date,month,dow) === true && this.dstStatus === false) {
-    // clocks go forward, CEST
-    this.i2c.writeTo(C.i2c_address,[C.hourReg, (dec2bcd(3))]);
-    hours = 3;
-    this.dstStatus = true;
-  }
-
-  if (hours === 3 && minutes === 0 && seconds === 0 && this.isDST(date,month,dow) === false && this.dstStatus === true) {
-    // clocks go back, CET
-    this.i2c.writeTo(C.i2c_address,[C.hourReg, (dec2bcd(2))]);
-    hours = 2;
-    this.dstStatus = false;
-  }
-
   switch (select) {
     case "hours": return hours;
     case "minutes" : return minutes;
@@ -114,6 +75,6 @@ DS3231.prototype.readDateTime = function (select) {
   }
 };
 
-exports.connect = function(i2c, options) {
-  return new DS3231(i2c, options);
+exports.connect = function(i2c) {
+  return new DS3231(i2c);
 };
